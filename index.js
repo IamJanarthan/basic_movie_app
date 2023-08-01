7701490a66d59ec21271bb0fc4474b993cbfee75
Janarthan <114041173+IamJanarthan@users.noreply.github.com>
2023-08-01 21:08:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,152 @@
+// JavaScript code for the home page
+// (Trending movies, Recent Movies, and Upcoming Movies sections)
+
+// Your API Key from TMDb
+const apiKey = '829a43a98259bc44cae297489c7e3bba';
+
+const recentMoviesRow = document.getElementById('recentMoviesRow');
+const topRatedMoviesRow = document.getElementById('topRatedMoviesRow');
+const upcomingMoviesRow = document.getElementById('upcomingMoviesRow');
+
+const totalPages = 5; // Adjust the number of total pages you want to display
+
+// Function to fetch and display movie thumbnails for a specific category
+async function displayMovieThumbnails(category, container) {
+  for (let page = 1; page <= totalPages; page++) {
+    const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&region=IN&with_original_language=ta|hi|ka|te|ml&page=${page}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const movies = data.results;
+
+      // Process movie data and create thumbnail elements
+      movies.forEach(movie => {
+        const thumbnailElement = createThumbnailElement(movie);
+        container.appendChild(thumbnailElement);
+      });
+    } catch (error) {
+      console.error('Error fetching movie data:', error);
+    }
+  }
+}
+
+// Function to create a single movie thumbnail element
+function createThumbnailElement(movie) {
+  const thumbnailElement = document.createElement('div');
+  thumbnailElement.classList.add('homeThumbnail');
+
+  // Customize the thumbnail display as needed
+  thumbnailElement.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}">
+    <p class="title">${movie.title}</p>
+         
+          <div class="meta-list">
+               <div class="meta-item">
+                   <img
+                   src="./star.png"
+                   width="20"
+                   height="18"
+                   loading="lazy"
+                   alt="rating"
+                   />
+               <span class="span">${movie.vote_average.toFixed(1)}</span>
+           </div>
+
+           <div class="card-badge">${movie.release_date.split("-")[0]}</div>
+           </div>
+  `;
+
+
+   thumbnailElement.addEventListener('click', () => {
+   window.location.href = `movie_details.html?id=${movie.id}`;
+   });
+
+ 
+
+  return thumbnailElement;
+}
+
+// Call the function to display movie thumbnails for each row
+displayMovieThumbnails('now_playing', recentMoviesRow);
+displayMovieThumbnails('top_rated', topRatedMoviesRow);
+displayMovieThumbnails('upcoming', upcomingMoviesRow);
+
+
+
+
+
+
+// Add your search functionality here
+function searchMovies(query) {
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&region=IN&query=${query}&adult=false`;
+  return fetch(searchUrl)
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error fetching search results:', error);
+      throw error;
+    });
+}
+
+function displaySearchResults(movies) {
+  const searchResultsContainer = document.getElementById('searchResults');
+  searchResultsContainer.innerHTML = '';
+
+  movies.forEach(movie => {
+    const thumbnail = document.createElement('div');
+    thumbnail.classList.add('thumbnail');
+
+    const img = document.createElement('img');
+    img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    img.alt = 'Movie Poster';
+
+    thumbnail.appendChild(img);
+
+    thumbnail.addEventListener('click', () => {
+      window.location.href = `movie_details.html?id=${movie.id}`;
+    });
+
+    searchResultsContainer.appendChild(thumbnail);
+  });
+}
+
+ // Handle search button click event
+document.getElementById('searchButton').addEventListener('click', () => {
+  const searchInput = document.getElementById('searchInput');
+  const query = searchInput.value.trim();
+
+
+    
+    if (query !== '') {
+     
+      searchMovies(query)
+        .then(searchResults => {
+          displaySearchResults(searchResults.results);
+
+          console.log('Search button clicked!');
+
+          // Hide other sections and show search results section
+          document.getElementById('topRatedMoviesSection').style.display = 'none';
+          document.getElementById('recentMoviesSection').style.display = 'none';
+          document.getElementById('upcomingMoviesSection').style.display = 'none';
+          document.getElementById('searchResultsSection').style.display = 'block';
+        })
+        .catch(error => {
+          console.error('Error fetching search results:', error);
+        });
+    }
+  });
+
+  
+// Handle back button click event
+document.getElementById('backButton').addEventListener('click', () => {
+  // Show other sections and hide search results section
+  document.getElementById('topRatedMoviesSection').style.display = 'block';
+  document.getElementById('recentMoviesSection').style.display = 'block';
+  document.getElementById('upcomingMoviesSection').style.display = 'block';
+  document.getElementById('searchResultsSection').style.display = 'none';
+});
+
+
+
+
+
---
+++ movie_details.js
@@ -0,0 +1,73 @@
+// API Key from TMDb
+const apiKey = '829a43a98259bc44cae297489c7e3bba';
+
+// Fetch movie details by ID
+function fetchMovieDetails(movieId) {
+  const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=casts,videos,images,releases&language=en-US`;
+  return fetch(movieDetailsUrl)
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error fetching movie details:', error);
+      throw error;
+    });
+}
+
+function getLanguageName(code) {
+
+  const languageNames = {
+    en: 'English',
+    ta: 'Tamil',
+    hi: 'Hindi',
+    ka: 'Kannada',
+    te: 'Telugu',
+    ml: 'Malayalam'
+  };
+  
+  return languageNames[code] || code;
+}
+
+
+
+// Display movie details on the movie details page
+function displayMovieDetails(movie) {
+  const movieDetailsContainer = document.getElementById('movieDetails');
+  movieDetailsContainer.innerHTML = `
+    <div class="movie-details">
+      <div class="backdrop" style="background-image: url('https://image.tmdb.org/t/p/original${movie.backdrop_path}')"></div>
+      <div class="overlay"></div>
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Movie Poster" class="movie-poster">
+      <div class="movie-info">
+        <h2 id="movieTitle">${movie.title}</h2>
+        <p id="movieOverview"> <strong> Overview: </strong> ${movie.overview}</p>
+        <p id="movieProductionCompanies"> <strong> Production Companies: </strong> ${movie.production_companies.map(company => company.name).join(', ')}</p>
+        <p id="movieOriginalLanguage"> <strong> Original Language: </strong> ${getLanguageName(movie.original_language)}</p>
+        <p id="runtime"> <strong> Runtime: </strong> ${movie.runtime} mins</p>
+        <p id="avgVote"> <strong> Rating: </strong><img src="./star.png" width="20" height="15" loading="lazy" alt="rating"/>${movie.vote_average.toFixed(1)}</p>
+        <p id="movieReleaseDate"> <strong> Release Date: </strong> ${movie.release_date}</p>
+        <p id="movieReleaseYear"> <strong> Year: </strong> ${movie.release_date.split("-")[0]}</p>
+        <p id="movieGenre"> <strong> Genre: </strong> ${movie.genres.map(genre => genre.name).join(', ')}</p>  
+      </div>
+    </div>
+  `;
+  
+}
+
+
+
+// Fetch movie details when the page loads
+window.addEventListener('load', () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const movieId = urlParams.get('id');
+
+  if (movieId) {
+    fetchMovieDetails(movieId)
+      .then(movie => {
+        displayMovieDetails(movie);
+      })
+      .catch(error => {
+        console.error('Error fetching movie details:', error);
+      });
+  }
+});
+
